e29eeb4eddd3f499b2d4b397c2e7d60db45d6c18
Md. Abid Hasan Roni Bokshi <57047094+Roni95@users.noreply.github.com>
2022-12-10 20:22:44 +0600

    Create  3147 - The Battle of the Five Armies.c

---
+++ 3147 - The Battle of the Five Armies.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    int a,b,c,d,e,f;
+    scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+    if((a+b+c+f)>=(d+e))
+        printf("Middle-earth is safe.\n");
+    else
+        printf("Sauron has returned.\n");
+    return 0;
+}
